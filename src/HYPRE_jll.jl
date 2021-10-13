17407fc28520c9dadcfb2a933ec6f35e668c2a22
jlbuild <juliabuildbot@gmail.com>
2021-10-13 20:46:50 +0000

    HYPRE_jll build 2.23.0+0

---
+++ src/HYPRE_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule HYPRE_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("HYPRE")
+JLLWrappers.@generate_main_file("HYPRE", UUID("0a602bbd-b08b-5d75-8d32-0de6eef44785"))
+end  # module HYPRE_jll
