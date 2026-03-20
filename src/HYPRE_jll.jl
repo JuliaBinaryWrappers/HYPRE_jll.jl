b39fa363955bb9282227d9eefaaa3be69f68e9a3
jlbuild <juliabuildbot@gmail.com>
2026-03-20 15:57:59 +0000

    HYPRE_jll build 3.0.1+0

--- src/HYPRE_jll.jl
+++ src/HYPRE_jll.jl
@@ -8,5 +8,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("HYPRE")
-JLLWrappers.@generate_main_file("HYPRE", UUID("0a602bbd-b08b-5d75-8d32-0de6eef44785"))
+JLLWrappers.@generate_main_file("HYPRE", Base.UUID("0a602bbd-b08b-5d75-8d32-0de6eef44785"))
 end  # module HYPRE_jll
